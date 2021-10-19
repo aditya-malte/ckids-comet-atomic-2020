Fix labels argument error by changing transformers. Modify loss calculation accordingly

mosaic/infra/logging.py
@@ -32,12 +32,14 @@ def log_eval(epoch, tokenizer, model, device, loader, sample_limit=5000, model_c
             ids = data['source_ids'].to(device, dtype = torch.long)
             mask = data['source_mask'].to(device, dtype = torch.long)
             if model_class == "t5":
-                outputs = model(input_ids = ids, attention_mask = mask, decoder_input_ids=y_ids, lm_labels=lm_labels)
+                # outputs = model(input_ids = ids, attention_mask = mask, decoder_input_ids=y_ids, lm_labels=lm_labels)
+                outputs = model(input_ids = ids, attention_mask = mask, decoder_input_ids=y_ids)
             else:
                 outputs = model(input_ids = ids, attention_mask = mask, labels=ids)
 
             loss = outputs[0]
-            total_loss += loss.item()
+            # total_loss += loss.item()
+            total_loss += loss.mean().item()
             loss_count += 1
     wandb.log({"Eval Loss": total_loss / loss_count})
     logger.info("Eval Loss: {}".format(total_loss / loss_count))