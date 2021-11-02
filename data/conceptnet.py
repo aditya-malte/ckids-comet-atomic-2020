c8c96112070618f8ef56faa25cf1524bda48ce98
Aditya Malte <aditya.malte@gmail.com>
2021-11-02 15:08:07 -0700

    Fix splitting bug

--- data/conceptnet.py
+++ data/conceptnet.py
@@ -7,7 +7,7 @@ DATA_DIR = "/nas/home/malte/ckids-comet-atomic-2020/data"
 
 data = (pd.read_csv(FILE_PATH, sep="\t")[["node1;label", "relation;label", "node2;label"]]).dropna()
 train, dev, test = np.split(data.sample(frac=1, random_state=42).sample(frac=1, random_state=42), 
-                       [int(.8*len(data)), int(.1*len(data))])
+                       [int(0.8*len(data)), int(0.9*len(data))])
 
 train.to_csv(join(DATA_DIR, 'train.tsv'), index=None, sep="\t", header=None)
 dev.to_csv(join(DATA_DIR,'dev.tsv'), index=None, sep="\t", header=None)
