5a54c9c58dd9f7f5274d0651959824c54995093b
Aditya Malte <aditya.malte@gmail.com>
2021-11-02 14:45:55 -0700

    ConceptNet splits generation

---
+++ data/conceptnet.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from os.path import join
+
+FILE_PATH = "/data/malte/kgtk_conceptnet.tsv"
+DATA_DIR = "/nas/home/malte/ckids-comet-atomic-2020/data"
+
+data = pd.read_csv(FILE_PATH, sep="\t")[["node1;label", "relation;label", "node2;label"]]
+train, dev, test = np.split(data.sample(frac=1, random_state=42).sample(frac=1, random_state=42), 
+                       [int(.8*len(data)), int(.1*len(data))])
+
+train.to_csv(join(DATA_DIR, 'train.tsv'), index=None, sep="\t", header=None)
+dev.to_csv(join(DATA_DIR,'dev.tsv'), index=None, sep="\t", header=None)
+test.to_csv(join(DATA_DIR,'test.tsv'), index=None, sep="\t", header=None)
