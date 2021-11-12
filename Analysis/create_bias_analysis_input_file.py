6ad35986ef175d1fbfe8f5067dcd57f92ce9122d
CathyZhang0107 <62780564+CathyZhang0107@users.noreply.github.com>
2021-11-11 21:40:27 -0800

    Create create_bias_analysis_input_file.py

---
+++ Analysis/create_bias_analysis_input_file.py
@@ -0,0 +1,50 @@
+import pandas as pd 
+import csv
+
+# This file creates a text file for use in the bias (sentiment + regard) classification
+# Currently, it only supports ConceptNet (where relations are already in NL)
+
+# global variables 
+DATA_PATH = '/nas/home/linglan/ckids-comet-atomic-2020/data/'
+DATA_OUTPUT_PATH = '/nas/home/linglan/ckids-comet-atomic-2020/data/'
+COMET_OUTPUT_FILE_NAME = 'wiki_output_clean.tsv'
+KG = 'wikidata'
+
+# read predictions as dataframe
+def get_predictions(file_name):
+    return pd.read_csv(file_name, names = ['head_event', 'relation', 'tail_event'], encoding='latin-1', sep="\t")
+
+# write unmasked version of comet predictions
+def get_unmasked_version(keyword, relation, tail):
+    if pd.isna(tail):
+        tail = ''
+    return keyword.lower() + '\t' + relation.lower() + '\t' + tail.lower().strip()
+
+# write masked version of comet predictions
+def get_masked_version(relation, tail):
+    if pd.isna(tail):
+        tail = ''
+    return 'this person' + '\t' + relation.lower() + '\t' + tail.lower().strip()
+
+# creates conditions on which a particular line should be skipped
+def should_skip_line(keyword, relation, tail):
+    # if relation == 'defined as':
+    #     return True
+    return False
+
+if __name__ == '__main__':
+
+    preds = get_predictions(DATA_PATH + COMET_OUTPUT_FILE_NAME)
+
+    with open(DATA_OUTPUT_PATH + KG + '_bias_input.txt', 'w+') as bias_input_file:
+        for index, row in preds.iterrows():
+            keyword = row['head_event']
+            relation = row['relation']
+            tail = row['tail_event']
+
+            if should_skip_line(keyword, relation, tail) == False:
+                if index != 0:
+                    bias_input_file.write('\n')
+                bias_input_file.write(get_unmasked_version(keyword, relation, tail))
+                bias_input_file.write('\n')
+                bias_input_file.write(get_masked_version(relation, tail))
